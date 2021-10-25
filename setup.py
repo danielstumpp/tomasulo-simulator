9cd0d0117772cf0be817bf03922930584841bb57
Daniel <dcs98@pitt.edu>
2021-10-25 13:19:28 -0400

    testing CI

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simulator',
+    version='1.0',
+    description='A tomasulo simulator module',
+    author='Daniel Stumpp, Colman Glagovich, Owen Lucas',
+    author_email='dcs98@pitt.edu',
+    packages=['simulator'],  # same as name
+    # external packages as dependencies
+    install_requires=['pytest', 'pytest-cov', 'coveralls'],
+)
---
+++ tests/test_multiple.py
@@ -0,0 +1,12 @@
+from simulator.modules.maths import multiple, add
+
+
+def test_multiple_3_4():
+    assert multiple(3, 4) == 12
+
+
+def test_add_3_4():
+    assert add(3, 4) == 7
+
+
+print(add(4,5))
