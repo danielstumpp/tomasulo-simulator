6f3d82d663ea72228da2ee97266bc08d0e7cea2a
Daniel <dcs98@pitt.edu>
2021-10-26 11:52:54 -0400

    Added state class and initialized memory and reg w/ tests to verify

---
+++ simulator/modules/state.py
@@ -0,0 +1,16 @@
+""" Defines the relevant objects for saving the state of the simulator"""
+
+
+class State:
+    """ Simulator State Object"""
+
+    def __init__(self):
+        """ Init Simulator State Object"""
+
+        # initialize the system registers to zero
+        register_keys = ["{}{}".format('R', i) for i in list(
+            range(32))] + ["{}{}".format('F', j) for j in list(range(32))]
+        self.registers = dict.fromkeys(register_keys, 0)
+
+        # initialize the system memory to zero
+        self.memory = [0] * 256
---
+++ tests/test_state.py
@@ -0,0 +1,14 @@
+from simulator.modules import state
+
+def test_state_mem_init():
+    test_state = state.State()
+    assert len(test_state.memory) == 256
+    for i in range(len(test_state.memory)):
+        assert test_state.memory[i] == 0
+
+
+def test_state_reg_init():
+    test_state = state.State()
+    assert len(test_state.registers) == 32 * 2
+    for k in test_state.registers.keys():
+        assert test_state.registers[k] == 0
