3bbf5295a88dfa1e1d87691be29e1551e07a7da7
Daniel <dcs98@pitt.edu>
2021-11-12 14:15:41 -0500

    SUB single inst test

--- tests/test_single_inst.py
+++ tests/test_single_inst.py
@@ -23,3 +23,14 @@ def test_add_one_inst():
     assert tt_gold == tt_test
     assert state.registers['R3'] == 30
     assert list(state.RAT.values()) == list(state.RAT.keys())
+
+
+def test_sub_one_inst():
+    state = simulator.run('tests/inputs/config/sub_single.yml')
+    tt_test = TimingTable()
+    tt_gold = TimingTable()
+    tt_test.load_from_state(state)
+    tt_gold.load_from_file('tests/inputs/timing/sub_single.tt')
+    assert tt_gold == tt_test
+    assert state.registers['R3'] == -10
+    assert list(state.RAT.values()) == list(state.RAT.keys())
