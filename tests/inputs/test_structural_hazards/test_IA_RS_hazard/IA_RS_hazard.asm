743fe24e16941464e93eb3883cabcfb451029252
Daniel <dcs98@pitt.edu>
2021-11-13 15:18:55 -0500

    structural hazard tests...

---
+++ tests/inputs/test_structural_hazards/test_IA_RS_hazard/IA_RS_hazard.asm
@@ -0,0 +1,4 @@
+addi, R5, R1, 10
+add, R6, R2, R3
+sub, R7, R4, R3
+sub, R8, R3, R4
--- tests/test_structural_hazards.py
+++ tests/test_structural_hazards.py
@@ -2,3 +2,18 @@ from simulator import simulator
 from simulator.modules.timing_table import TimingTable
 
 root = 'tests/inputs/test_structural_hazards/'
+
+
+def test_IA_RS_hazard():
+    state = simulator.run(root + 'test_IA_RS_hazard/IA_RS_hazard.yml')
+    tt_test = TimingTable()
+    tt_gold = TimingTable()
+    tt_test.load_from_state(state)
+    tt_gold.load_from_file(root + 'test_IA_RS_hazard/IA_RS_hazard.tt')
+    print(tt_test)
+    assert list(state.RAT.values()) == list(state.RAT.keys())
+    assert state.registers['R5'] == 20
+    assert state.registers['R6'] == 50
+    assert state.registers['R7'] == -40
+    assert state.registers['R8'] == 40
+    assert tt_gold == tt_test
