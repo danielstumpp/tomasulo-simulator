730a9b02fd9a24c1bae5b6251228c1cc5da9c6c5
Daniel <dcs98@pitt.edu>
2021-11-13 12:27:44 -0500

    Re-structured testing directory for organization by test

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -11,31 +11,31 @@ def test_bad_config_filename():
 
 
 def test_good_config_filename():
-    fn = 'tests/inputs/config/test1_config.yml'
+    fn = 'tests/inputs/test_parsers/test1_config.yml'
     state = State()
     assert psr.load_config(state, fn) == True
 
 
 def test_bad_memory_filename():
-    fn = 'tests/inputs/mem/non-existant-mem-file.mem'
+    fn = 'tests/inputs/test_parsers/non-existant-mem-file.mem'
     state = State()
     assert psr.init_memory(state, fn) == False
 
 
 def test_out_of_range_address():
-    fn = 'tests/inputs/mem/bad_out_of_range.mem'
+    fn = 'tests/inputs/test_parsers/bad_out_of_range.mem'
     state = State()
     assert psr.init_memory(state, fn) == False
 
 
 def test_non_numeric_entry():
-    fn = 'tests/inputs/mem/bad_non_numeric.mem'
+    fn = 'tests/inputs/test_parsers/bad_non_numeric.mem'
     state = State()
     assert psr.init_memory(state, fn) == False
 
 
 def test_good_mem_init():
-    fn = 'tests/inputs/mem/good_test.mem'
+    fn = 'tests/inputs/test_parsers/good_test.mem'
     state = State()
     assert psr.init_memory(state, fn) == True
     # looping through byte addresses
@@ -44,25 +44,25 @@ def test_good_mem_init():
 
 
 def test_bad_register_filename():
-    fn = 'tests/inputs/reg/bad-reg-filename.reg'
+    fn = 'tests/inputs/test_parsers/bad-reg-filename.reg'
     state = State()
     assert psr.init_registers(state, fn) == False
 
 
 def test_bad_register_name():
-    fn = 'tests/inputs/reg/bad_invalid_name.reg'
+    fn = 'tests/inputs/test_parsers/bad_invalid_name.reg'
     state = State()
     assert psr.init_registers(state, fn) == False
 
 
 def test_bad_int_value():
-    fn = 'tests/inputs/reg/bad_int_value.reg'
+    fn = 'tests/inputs/test_parsers/bad_int_value.reg'
     state = State()
     assert psr.init_registers(state, fn) == False
 
 
 def test_good_reg_inti():
-    fn = 'tests/inputs/reg/good_test.reg'
+    fn = 'tests/inputs/test_parsers/good_test.reg'
     state = State()
     assert psr.init_registers(state, fn) == True
     assert state.registers['F1'] == 1.99
@@ -101,7 +101,7 @@ def test_validate_instruction_BNE_BEQ():
     assert psr.validate_instruction(il, inst) == False
 
 def test_parse_instructions_good():
-    fn = 'tests/inputs/inst/inst1.asm'
+    fn = 'tests/inputs/test_parsers/inst1.asm'
     state = State()
     assert psr.parse_instructions(state, fn) == True
     assert len(state.instructions) == 3
@@ -118,7 +118,7 @@ def test_parse_instructions_good():
     
 
 def test_good_config():
-    fn = 'tests/inputs/config/test1_config.yml'
+    fn = 'tests/inputs/test_parsers/test1_config.yml'
     state = State()
     assert psr.load_config(state,fn) == True
     
