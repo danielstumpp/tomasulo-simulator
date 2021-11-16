0e5d3a118c923ccb9f9b8b960da3f4de6d1147f4
unknown <opl1@pitt.edu>
2021-11-16 17:00:11 -0500

    Added robust branch predictor test

--- tests/test_branch_prediction.py
+++ tests/test_branch_prediction.py
@@ -43,3 +43,16 @@ def test_bne_not_taken():
     assert tt_gold == tt_test
     assert state.registers['R1'] == 7
     assert list(state.RAT.values()) == list(state.RAT.keys())
+
+def test_robust_pred():
+    state = simulator.run(root + 'test_robust_pred/test.yml')
+    tt_test = TimingTable()
+    tt_gold = TimingTable()
+    tt_test.load_from_state(state)
+    tt_gold.load_from_file(root + 'test_robust_pred/test.tt')
+    #assert tt_gold == tt_test
+    assert state.registers['R1'] == 3
+    assert state.registers['R3'] == 6
+    assert state.registers['R4'] == 8
+    assert state.registers['R5'] == 10
+    assert list(state.RAT.values()) == list(state.RAT.keys())
