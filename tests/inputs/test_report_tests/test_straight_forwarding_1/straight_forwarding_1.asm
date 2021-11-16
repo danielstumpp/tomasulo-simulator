976dff31b9090e34e4866ffd746e12ac388214c9
Daniel <dcs98@pitt.edu>
2021-11-16 10:25:00 -0500

    store forwarding test

---
+++ tests/inputs/test_report_tests/test_straight_forwarding_1/straight_forwarding_1.asm
@@ -0,0 +1,6 @@
+ld, F1, 0(R10)
+mult.d, F2, F1, F5
+sd, F2, 8(R0)
+ld, F3, 8(R0)
+ld, F4, 4(R10)
+ld, F5, 8(R0)
--- tests/test_report_tests.py
+++ tests/test_report_tests.py
@@ -22,3 +22,24 @@ def test_straight_dep_1():
     tt_gold.load_from_file(root + 'test_straight_dep_1/straight_dep_1.tt')
     assert list(state.RAT.values()) == list(state.RAT.keys())
     assert tt_gold == tt_test
+    assert state.registers['R10'] == 10
+    assert state.registers['R11'] == 20
+    assert state.registers['F10'] == 30
+    assert state.registers['F11'] == 50
+    assert state.registers['R20'] == 11
+    assert state.registers['R21'] == 1
+    assert state.registers['F20'] == 20
+    assert state.registers['F31'] == -30
+    assert state.memory[4] == 1
+    assert state.memory[5] == -30
+    assert state.memory[3] == 40
+    
+
+def test_straight_forwarding_1():
+    state = simulator.run(root + 'test_straight_forwarding_1/straight_forwarding_1.yml')
+    tt_test = TimingTable()
+    tt_gold = TimingTable()
+    tt_test.load_from_state(state)
+    tt_gold.load_from_file(root + 'test_straight_forwarding_1/straight_forwarding_1.tt')
+    assert list(state.RAT.values()) == list(state.RAT.keys())
+    assert tt_gold == tt_test
